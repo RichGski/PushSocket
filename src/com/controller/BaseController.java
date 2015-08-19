74523baff788e9a5512e1d023e1296d10c8a9c13
richgski <richgski@gmail.com>
2015-08-19 13:01:00 -0700

    Initial Commit

--- src/com/controller/BaseController.java
+++ src/com/controller/BaseController.java
@@ -1,15 +1,52 @@
 package com.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.system.MainControllerWebSocket;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BaseController {
+	/**
+	 * Sends the passed message to the session using web socket
+	 * @param strMessage	The message to send to the connected user
+	 */
+	private void sendMessage(String strMessage, HttpServletRequest request){
+		MainControllerWebSocket.sendMessage(strMessage, request.getSession().getId());
+	}
 	
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld() {
-	String message = "<br><div align='center'>" + "<h3>********** Hello World! **********<br><br>";
+	private void wait(int iDelay){
+		try {
+			Thread.sleep(iDelay);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	
+	@RequestMapping("/test")
+	public ModelAndView test() {
+		return new ModelAndView("main");
+	}
+	
+		
+	@RequestMapping("/testwebsocketpush")
+	public ModelAndView helloWorld(HttpServletRequest request) {
+	sendMessage("Starting, please wait.", request);
+	wait(500);
+	sendMessage("One.", request);
+	wait(500);
+	sendMessage("Two.", request);
+	wait(1000);
+	sendMessage("Three.", request);
+	wait(500);
+	sendMessage("Four.", request);
+	wait(1000);
+	String message = "<br><div align='center'>" + "<h3>********** Done! **********<br><br>";
 	return new ModelAndView("welcome", "message", message);
 	}
 }
